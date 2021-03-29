c7f9573d033a7e277fd8e713b350afed605dac96
Nataly-vergara <vergara.26@osu.edu>
2021-03-28 21:15:05 -0500

    hover propeties
    spent alot of time attempting to creat a layer of draggable png's across the site. it wasnt working so. did alot of hover

---
+++ crashcourse/main.js
@@ -0,0 +1,8 @@
+//arrays - varriables hold multiple values 
+const fruits= ['apples', 'oranges', 'pears']
+
+fruits[3] ='grapes';
+
+console.log(fruits[0])
+
+
--- exercises/ex-4/main.js
+++ exercises/ex-4/main.js
@@ -1,5 +1,18 @@
+const draggables = document.querySelectorALL ('.draggable')
+containers = document.querySelectorAll('.container')
 
-    (".sunset").click(function () {
-            (".sunset").hide();
-        });
-  });
+draggables.array.forEach(draggable => { 
+    draggable.addEventListner('dragstart', () => {
+        draggable.classList.add('dragging')
+    })
+    
+    draggable.addEventListner('dragend', () => {
+        draggable.classList.remove ('dragging')
+    })
+})
+containers.forEach(container => { 
+    container.addEventListner('dragover', ()  => {.preventDefault()
+        const draggable = document.querySelector ('dragging')
+        container.appendChild(draggable) 
+    })
+})
