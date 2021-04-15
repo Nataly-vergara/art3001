cafb5d50cc5ec0a913cd885bbc2dadc0eaddad49
Nataly-vergara <vergara.26@osu.edu>
2021-04-15 09:21:16 -0500

    trying to make an unploadable file section

---
+++ exercises/ex-6/ex-6 file upload/uploadfile.php
@@ -0,0 +1,5 @@
+<?php $ path = "files/"; $path = $path . basename( $_FILES['userfile']['name']);
+
+if(move_uploaded_file($_FILES['userfile']['tmp_name'], $path)) { echo "Success uploading". basename($_FILES['userfile']['name']); } else{ echo "Error when uploading file."; } ?>
+
+<!-- https://www.techwalla.com/articles/http-vs-ftp-file-transfer  -->
